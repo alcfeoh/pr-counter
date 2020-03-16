ba1b7377fba25807a3025520891142f4761d4529
alcfeoh <alain@interstate21.com>
2020-03-16 10:26:41 -0700

    Added more amounts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,5 +8,5 @@ import { Component } from '@angular/core';
 export class AppComponent  {
   totalAmount = 0;
   totalReviews = 0;
-  values = [8.25, 8.25 * 2, 8.25 * 3, 8.25 * 4, 8.25 * 5];
+  values = [8.25, 8.25 * 2, 8.25 * 3, 8.25 * 4, 8.25 * 5, 8.25 * 6, 8.25 *7, 8.25 *8, 8.25 *9, 8.25 *10];
 }
